dfab5a998bbda66eab6a97f65399bfccfda20caf
Mika Wahlroos <mika.wahlroos@iki.fi>
2019-06-09 21:51:28 +0300

    Show the terminal executable name in the context menu item
    This should help differentiate the customizable menu item of Nautiterm
    from other potentially similar menu items, such as the one added by
    Gnome Terminal extension.

--- src/nautiterm/open_terminal.py
+++ src/nautiterm/open_terminal.py
@@ -87,14 +87,14 @@ class OpenTerminalExtension(Nautilus.MenuProvider, GObject.GObject):
             return
 
         item = Nautilus.MenuItem(name='NautilusPython::openterminal_file_item',
-                                 label='Open Terminal',
+                                 label='Open Terminal (%s)' % self._get_terminal_exec(),
                                  tip='Open Terminal In %s' % file.get_name())
         item.connect('activate', self.menu_activate_cb, file)
         return item,
 
     def get_background_items(self, window, file):
         item = Nautilus.MenuItem(name='NautilusPython::openterminal_file_item2',
-                                 label='Open Terminal',
+                                 label='Open Terminal (%s)' % self._get_terminal_exec(),
                                  tip='Open Terminal In %s' % file.get_name())
         item.connect('activate', self.menu_background_activate_cb, file)
         return item,
